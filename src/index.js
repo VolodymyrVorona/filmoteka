2922e310b8726124ee1dab6ea5dbf9c4cd89e5eb
Halyna Pryn <prynhalyna@gmail.com>
2021-01-27 20:01:59 +0100

    Start writing fetch for main link

--- src/index.js
+++ src/index.js
@@ -1,49 +1,43 @@
 import './scss/main.scss';
+import trendMovieTemplate from './templates/trendMovieTemplate.hbs';
 
 const refs = {
   linkHome: document.querySelector('.home'),
   linkMyLibrary: document.querySelector('.library'),
-  linkMyLibrary: document.querySelector('header .logo'),
+  listMovie: document.querySelector('.trend-movie'),
 };
 
+onHome();
+
 refs.linkHome.addEventListener('click', onHome);
 refs.linkHome.addEventListener('click', onHome);
-// refs.linkMyLibrary.addEventListener('click', onLibrary);
+refs.linkMyLibrary.addEventListener('click', onLibrary);
 
 function onHome() {
-  return fetch(
+  refs.linkHome.classList.add('active');
+  refs.linkMyLibrary.classList.remove('active');
+  fetch(
     'https://api.themoviedb.org/3/trending/movie/day?api_key=bf08c0c07642287cbabe383c02818eb3',
   )
     .then(response => response.json())
     .then(data => data.results)
     .then(movies => {
-      const marcup = film(movies);
-      movieRef.insertAdjacentHTML('beforeend', marcup);
-      let filmItemRef = document.querySelectorAll('.film-item');
-      let genreRef = document.querySelectorAll('.genre');
-      filmItemRef.forEach(data => {
-        let filmID = data.dataset.film;
-        // console.log(filmID);
-        // fetch(`https://api.themoviedb.org/3/movie/${filmID}?api_key=bf08c0c07642287cbabe383c02818eb3`).then(response =>
-        //     response.json())
+      movies.forEach(movie => {
+        const idMovie = movie.id;
+        console.log(idMovie);
       });
-      genreRef.forEach(genre => {
-        // console.log(genre.textContent);
-        if (genre.textContent.includes(18)) {
-          genre.textContent = 'Drama';
-        }
-        if (genre.textContent.includes(28)) {
-          genre.textContent = 'Action';
-        }
-        if (genre.textContent.includes(10752)) {
-          genre.textContent = 'War';
-        }
-      });
-      // Обрезаем лишнее в дате, оставлем только год
-      let dataRef = document.querySelectorAll('p.data');
-      dataRef.forEach(data => {
-        let newData = data.textContent.slice(0, 5);
-        data.textContent = newData;
-      });
+
+      // fetch(
+      //   'https://developers.themoviedb.org/3/movies/get-movie-details?api_key=bf08c0c07642287cbabe383c02818eb3',
+      // );
+
+      const marcup = trendMovieTemplate(movies);
+      refs.listMovie.insertAdjacentHTML('afterbegin', marcup);
     });
+}
+
+function onLibrary(event) {
+  refs.linkMyLibrary.classList.add('active');
+  refs.linkHome.classList.remove('active');
+  refs.listMovie.innerHTML = '';
 }
