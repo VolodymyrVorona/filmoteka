e618d00a5d9decf763cce6da98ab98254de37667
VHromyk <hromykk@gmail.com>
2021-02-02 21:59:05 +0200

    commit to master

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import './scss/main.scss';
 import './JS/searchByKeywords';
 import './JS/mainPageMovie';
 
-import './JS/firebase';
-import './JS/authenticateUser';
-import './JS/signIn_modal';
-
+// import './JS/firebase';
+// import './JS/authenticateUser';
+// import './JS/signIn_modal';
 
 import './JS/searchByKeywords';
 import './JS/footer-modal-about-us';
