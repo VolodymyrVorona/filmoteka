76e00ea85594faba64ad01bdf76bf7d7a6144103
LettaSav <lettawetta@icloud.com>
2021-02-05 00:18:56 +0200

    auntification-done

--- src/JS/authenticateUser.js
+++ src/JS/authenticateUser.js
@@ -1,18 +1,53 @@
-import { auth } from './firebase';
+import firebase from 'firebase/app';
+import authFire from './firebase';
 
-const signUpbtn = document.querySelector('.signUpbtn');
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+const signInForm = document.querySelector('.formIn-group');
+const signUpForm = document.querySelector('.formUp-group');
 
-signUpbtn.addEventListener('click', authenticateUser(email, password, false));
+signInForm.addEventListener('submit', signInWithEmailPassword);
+signUpForm.addEventListener('submit', signUpWithEmailPassword);
 
-async function authenticateUser(email, password, isLogin) {
-  try {
-    const user = isLogin
-      ? await auth.signInWithEmailAndPassword(email, password)
-      : await auth.createUserWithEmailAndPassword(email, password);
-    console.log(user);
-  } catch (err) {
-    console.log(err);
-  }
+console.log(signInForm);
+console.log(signUpForm);
+
+function signInWithEmailPassword(event) {
+  event.preventDefault();
+  const email = document.querySelector('.signInemail').value;
+  const password = document.querySelector('.signInpassword').value;
+
+  console.log(email, password);
+
+  authFire.signInWithEmailAndPassword(email, password).catch(error => {
+    console.log(error);
+  });
 }
+
+function signUpWithEmailPassword(event) {
+  event.preventDefault();
+  const email = document.querySelector('.sign_email').value;
+  const password = document.querySelector('.sign_password').value;
+
+  authFire.createUserWithEmailAndPassword(email, password).catch(error => {
+    console.log(error);
+  });
+}
+
+let user = authFire.currentUser;
+authFire.onAuthStateChanged(_user => {
+  if (_user) {
+    user = _user;
+    console.log(user);
+    document.getElementById('btnLogOut').classList.remove('hide');
+    document.querySelector('.signUp').classList.add('hide');
+    document.querySelector('.signIn').classList.add('hide');
+  } else {
+    document.getElementById('btnLogOut').classList.add('hide');
+    document.querySelector('.signUp').classList.remove('hide');
+    document.querySelector('.signIn').classList.remove('hide');
+  }
+});
+
+document.getElementById('btnLogOut').addEventListener('click', e => {
+  authFire.signOut();
+  console.log('logged out');
+});
--- src/JS/signIn_modal.js
+++ src/JS/signIn_modal.js
@@ -1,14 +1,14 @@
-const signInbtn = document.querySelector('signIn');
-const signUpbtn = document.querySelector('signup');
-const sign_in_modal = document.querySelector('container-sign_in');
-const sign_up_modal = document.querySelector('container-sign_up');
+const signInbtn = document.querySelector('.signIn');
+const signUpbtn = document.querySelector('.signUp');
+const sign_in_modal = document.querySelector('.container-sign_in');
+const sign_up_modal = document.querySelector('.container-sign_up');
 
 signInbtn.addEventListener('click', signInOpen);
 signUpbtn.addEventListener('click', signUpOpen);
 
 function signInOpen() {
-  sign_in_modal.classList.remove('is-hidden');
+  sign_in_modal.classList.remove('is-unactive');
 }
 function signUpOpen() {
-  sign_in_modal.classList.remove('is-hidden');
+  sign_up_modal.classList.remove('is-unactive');
 }
